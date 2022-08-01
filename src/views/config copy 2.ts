dea3104898f2a2592a9f91ace36e2b6ba55591cb
HanRui <hi@hanrui.xyz>
2022-08-01 15:22:39 +0800

    1

---
+++ src/views/config copy 2.ts
@@ -0,0 +1,723 @@
+export const f_config = [
+  {
+    id: 1929,
+    planId: 346,
+    faceId: null,
+    trolleyTemplate: 1,
+    areaCode: "LA1",
+    areaName: "左一",
+    rowNums: null,
+    colNums: null,
+    circleNums: null,
+    offset: 0,
+    cutConsumption: 1.8,
+    auxiliaryConsumption: 1,
+    borderConsumption: 0.3,
+    computedParam: [
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: -230.5793773788124,
+        y: 193.47907051564837,
+        radius: 683,
+        startTheta: 3.0740582698382655,
+        endTheta: 2.7729937167839607,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: -867.7043685897426,
+          y: 439.5700000000003
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: -912.0224243161691,
+          y: 239.57000000000005
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -867.7043685897426,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 439.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 239.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 239.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -912.0224243161692,
+          y: 239.57
+        }
+      },
+      {
+        type: "POINT",
+        view: "SHOW",
+        x: -912.0224243161692,
+        y: 239.57
+      }
+    ]
+  },
+  {
+    id: 1930,
+    planId: 346,
+    faceId: null,
+    trolleyTemplate: 1,
+    areaCode: "LA2",
+    areaName: "左二",
+    rowNums: null,
+    colNums: null,
+    circleNums: null,
+    offset: 0,
+    cutConsumption: 1.8,
+    auxiliaryConsumption: 1,
+    borderConsumption: 0.3,
+    computedParam: [
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: -230.5793773788124,
+        y: 193.47907051564837,
+        radius: 683,
+        startTheta: 2.7729937167839607,
+        endTheta: 2.443460952792061,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: -753.7877320290743,
+          y: 632.5030079315549
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: -867.7043685897426,
+          y: 439.5700000000003
+        }
+      },
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: 0,
+        y: 0,
+        radius: 984,
+        startTheta: 2.443460952792061,
+        endTheta: 2.434048335181915,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: -747.8009194297638,
+          y: 639.5699999999999
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: -753.7877320290743,
+          y: 632.5030079315549
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -747.8009194297638,
+          y: 639.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 639.5699999999999
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 639.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 439.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -867.7043685897426,
+          y: 439.57
+        }
+      }
+    ]
+  },
+  {
+    id: 1931,
+    planId: 346,
+    faceId: null,
+    trolleyTemplate: 1,
+    areaCode: "LA3",
+    areaName: "左三",
+    rowNums: null,
+    colNums: null,
+    circleNums: null,
+    offset: 0,
+    cutConsumption: 1.8,
+    auxiliaryConsumption: 1,
+    borderConsumption: 0.3,
+    computedParam: [
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: 0,
+        y: 0,
+        radius: 984,
+        startTheta: 2.434048335181915,
+        endTheta: 2.2102723964656334,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: -587.2267152471862,
+          y: 789.57
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: -747.8009194297638,
+          y: 639.5699999999999
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -587.2267152471862,
+          y: 789.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 789.5699999999999
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 789.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 639.5699999999999
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 639.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -747.8009194297638,
+          y: 639.5699999999999
+        }
+      }
+    ]
+  },
+  {
+    id: 1932,
+    planId: 346,
+    faceId: null,
+    trolleyTemplate: 1,
+    areaCode: "VA",
+    areaName: "拱顶",
+    rowNums: null,
+    colNums: null,
+    circleNums: null,
+    offset: 0,
+    cutConsumption: 1.8,
+    auxiliaryConsumption: 1,
+    borderConsumption: 0.3,
+    computedParam: [
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: 0,
+        y: 0,
+        radius: 984,
+        startTheta: 2.2102723964656334,
+        endTheta: 0.9313202571241596,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 587.2267152471865,
+          y: 789.5699999999998
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: -587.2267152471862,
+          y: 789.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 587.2267152471862,
+          y: 789.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -587.2267152471862,
+          y: 789.5699999999999
+        }
+      }
+    ]
+  },
+  {
+    id: 1933,
+    planId: 346,
+    faceId: null,
+    trolleyTemplate: 1,
+    areaCode: "RA3",
+    areaName: "右三",
+    rowNums: null,
+    colNums: null,
+    circleNums: null,
+    offset: 0,
+    cutConsumption: 1.8,
+    auxiliaryConsumption: 1,
+    borderConsumption: 0.3,
+    computedParam: [
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: 0,
+        y: 0,
+        radius: 984,
+        startTheta: 0.9313202571241596,
+        endTheta: 0.7075443184078782,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 747.8009194297639,
+          y: 639.5699999999999
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 587.2267152471865,
+          y: 789.5699999999998
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 747.8009194297638,
+          y: 639.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 639.5699999999999
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 639.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 789.5699999999999
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 789.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 587.2267152471862,
+          y: 789.5699999999999
+        }
+      }
+    ]
+  },
+  {
+    id: 1934,
+    planId: 346,
+    faceId: null,
+    trolleyTemplate: 1,
+    areaCode: "RA2",
+    areaName: "右二",
+    rowNums: null,
+    colNums: null,
+    circleNums: null,
+    offset: 0,
+    cutConsumption: 1.8,
+    auxiliaryConsumption: 1,
+    borderConsumption: 0.3,
+    computedParam: [
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: 0,
+        y: 0,
+        radius: 984,
+        startTheta: 0.7075443184078782,
+        endTheta: 0.6981317007977318,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 753.7877320290744,
+          y: 632.5030079315546
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 747.8009194297639,
+          y: 639.5699999999999
+        }
+      },
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: 230.5793773788124,
+        y: 193.47907051564837,
+        radius: 683,
+        startTheta: 0.6981317007977318,
+        endTheta: 0.3685989368058325,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 867.7043685897426,
+          y: 439.57000000000016
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 753.7877320290744,
+          y: 632.5030079315546
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 867.7043685897426,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 439.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 639.5699999999999
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 639.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 747.8009194297638,
+          y: 639.5699999999999
+        }
+      }
+    ]
+  },
+  {
+    id: 1935,
+    planId: 346,
+    faceId: null,
+    trolleyTemplate: 1,
+    areaCode: "RA1",
+    areaName: "右一",
+    rowNums: null,
+    colNums: null,
+    circleNums: null,
+    offset: 0,
+    cutConsumption: 1.8,
+    auxiliaryConsumption: 1,
+    borderConsumption: 0.3,
+    computedParam: [
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: 230.5793773788124,
+        y: 193.47907051564837,
+        radius: 683,
+        startTheta: 0.3685989368058325,
+        endTheta: 0.06753438375152765,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 912.0224243161691,
+          y: 239.56999999999996
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 867.7043685897426,
+          y: 439.57000000000016
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 912.0224243161692,
+          y: 239.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 239.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 239.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 439.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 867.7043685897426,
+          y: 439.57
+        }
+      },
+      {
+        type: "POINT",
+        view: "SHOW",
+        x: 912.0224243161692,
+        y: 239.57
+      }
+    ]
+  },
+  {
+    id: 1936,
+    planId: 346,
+    faceId: null,
+    trolleyTemplate: 1,
+    areaCode: "CA",
+    areaName: "中部",
+    rowNums: null,
+    colNums: null,
+    circleNums: null,
+    offset: 0,
+    cutConsumption: 1.8,
+    auxiliaryConsumption: 1,
+    borderConsumption: 0.3,
+    computedParam: [
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 239.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 239.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 239.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 439.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "HIDDEN",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 439.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 239.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "OUT_OF_BAND",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 310,
+          y: 639.5699999999999
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: -310,
+          y: 639.5699999999999
+        }
+      }
+    ]
+  }
+]
---
+++ src/views/config copy.ts
@@ -0,0 +1,94 @@
+export const f_config = [
+  {
+    id: 1929,
+    planId: 346,
+    faceId: null,
+    trolleyTemplate: 1,
+    areaCode: "LA1",
+    areaName: "左一",
+    rowNums: null,
+    colNums: null,
+    circleNums: null,
+    offset: 0,
+    cutConsumption: 1.8,
+    auxiliaryConsumption: 1,
+    borderConsumption: 0.3,
+    computedParam: [
+      {
+        type: "ARC",
+        view: "SHOW",
+        x: 230.5793773788124,
+        y: 193.47907051564837,
+        radius: 683,
+        startTheta: 3.0740582698382655,
+        endTheta: 2.7729937167839607,
+        endPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 867.7043685897426,
+          y: 439.5700000000003
+        },
+        startPoint: {
+          type: "POINT",
+          view: "SHOW",
+          x: 912.0224243161691,
+          y: 239.57000000000005
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 867.7043685897426,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 1425.408,
+          y: 439.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 1425.408,
+          y: 439.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 1425.408,
+          y: 239.57
+        }
+      },
+      {
+        type: "LINE",
+        view: "SHOW",
+        p1: {
+          type: "POINT",
+          view: "SHOW",
+          x: 1425.408,
+          y: 239.57
+        },
+        p2: {
+          type: "POINT",
+          view: "SHOW",
+          x: 912.0224243161692,
+          y: 239.57
+        }
+      },
+      {
+        type: "POINT",
+        view: "SHOW",
+        x: 912.0224243161692,
+        y: 239.57
+      }
+    ]
+  }
+]
